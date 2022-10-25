1f6b2e45bf573300fa0419e18ac46bc1ede5a2d4
Martin Suarez <martin086@gmail.com>
2022-10-25 14:46:37 -0300

    Pulimos funcionalidad completa del carrito junto con ajustes de diseño.

--- scripts/carrito.js
+++ scripts/carrito.js
@@ -12,7 +12,6 @@ const total = document.getElementById('total');
 const iva = document.getElementById('iva');
 
 
-
 //Función para renderizar el catálogo de productos.
 function renderizarCatalogo() {
     productos.forEach((producto) => {
@@ -53,18 +52,25 @@ const agregarAlCarrito = (id) => {
 }
 
 //Función para calcular resúmen con el total de la compra.
+const envioSelect = document.getElementById('envioSelect');
 const calculoTotal = () => {
     let totalCompra = 0; 
     carrito.forEach((producto) => {
         totalCompra += producto.precio * producto.cantidad;
     })
-    const envioSelect = document.getElementById('envioSelect');
-    const envio = parseInt(envioSelect.value);
+    
+    const envio = parseInt(envioSelect[envioSelect.selectedIndex].value);
     const totalIva = parseFloat(((totalCompra + envio) * 0.21).toFixed(2));
     subTotalItems.innerHTML = `${moneda}${totalCompra}`;
     iva.innerHTML = `${moneda}${totalIva}`;
-    total.innerHTML = `${moneda}${(totalCompra+envio+totalIva)}`;
+    total.innerHTML = `${moneda}${parseFloat(totalCompra+envio+totalIva).toFixed(2)}`;
+    const xItems = document.getElementById('xItems');
+    xItems.innerHTML = `${carrito.length} item(s)`;
 }
+
+envioSelect.addEventListener('click', ()=>{
+    calculoTotal();
+});
 
 //Función para mostrar el carrito.
 const mostrarCarrito = () => {
@@ -83,43 +89,72 @@ const mostrarCarrito = () => {
                 <h6 class="text-black mb-0">${producto.tipo}</h6>
             </div>
             <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
-                <button class="btn btn-link px-2"
+                <button id="minusBtn${producto.idProducto}" class="btn btn-link px-2"
                     onclick="this.parentNode.querySelector('input[type=number]').stepDown()">
                     <i class="fas fa-minus"></i>
                 </button>
             
-                <input id="form1" min="1" name="quantity" value="1" type="number"
-                    class="form-control form-control-sm"/>
+                <input id="cantidad${producto.idProducto}" min="1" name="quantity" value="${producto.cantidad}" type="number"
+                    class="form-control form-control-sm text-center col-sm-2"/>
             
-                <button class="btn btn-link px-2"
+                <button id="plusBtn${producto.idProducto}" class="btn btn-link px-2"
                     onclick="this.parentNode.querySelector('input[type=number]').stepUp()">
                     <i class="fas fa-plus"></i>
                 </button>
             </div>
             <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
-                <h6 class="mb-0">${moneda}${producto.precio}</h6>
+                <h6 class="mb-0 text-end">${moneda}${producto.precio}</h6>
             </div>
             <div class="col-md-1 col-lg-1 col-xl-1 text-end">
-                <button id="del${producto.idProducto}" href="#!" class="text-muted"><i class="fas fa-times"></i></button>
+                <button id="del${producto.idProducto}" href="#!" class="btn btn-link px-2 text-muted"><i class="fas fa-times"></i></button>
             </div>  
         </div>
         <hr class="my-4">
         `
         carritoItems.appendChild(elementoCart);
         
+        //Incrementar o Dismunir cantidad de un mismo item:
+        const minusBtn = document.getElementById(`minusBtn${producto.idProducto}`);
+        const plusBtn = document.getElementById(`plusBtn${producto.idProducto}`);
+        let cantidad = document.getElementById(`cantidad${producto.idProducto}`);
+        let cantActual = cantidad.value;
+
+        plusBtn.addEventListener('click', ()=>{
+            cantActual++;
+            cantidad.value = cantActual;
+            producto.cantidad = parseInt(cantidad.value);
+            console.log(cantActual);
+            calculoTotal();
+            localStorage.setItem("carrito", JSON.stringify(carrito));
+        });
+        
+        minusBtn.addEventListener('click', ()=>{
+            cantActual--;
+            if(cantActual <= 1){
+                cantActual = 1;
+            }
+            cantidad.value = cantActual;
+            producto.cantidad = parseInt(cantidad.value);
+            console.log(cantActual);
+            calculoTotal();
+            localStorage.setItem("carrito", JSON.stringify(carrito));
+        });
+
         //Eliminar productos del carrito:
-        const botonDel = document.getElementById(`del${producto.idProducto}`);
-        botonDel.addEventListener('click', ()=>{
+        const delBtn = document.getElementById(`del${producto.idProducto}`);
+        delBtn.addEventListener('click', ()=>{
             eliminarDelCarrito(producto.idProducto);
         })
     })
     calculoTotal();
 }
 
+
 //Función para eliminar un producto del carrito:
 function eliminarDelCarrito(id) {
     const producto = carrito.find((producto) => producto.idProducto === id);
     const indice = carrito.indexOf(producto);
+    producto.cantidad = 1;
     carrito.splice(indice, 1);
     mostrarCarrito();
 
@@ -129,15 +164,14 @@ function eliminarDelCarrito(id) {
 
 //Vaciamos todo el carrito de compras: 
 const vaciarCarrito = document.getElementById('vaciarCarrito');
-
-//Función para eliminar todo el carrito: 
+//Función para vaciar todo el carrito: 
 const vaciarElCarrito = () => {
     carrito = [];
     mostrarCarrito();
     calculoTotal();
     localStorage.clear();
 }
-
+//Llamamos a la función de vaciado cuando el usuario clickea.
 vaciarCarrito.addEventListener('click', () => {
     vaciarElCarrito();
 })
@@ -149,7 +183,10 @@ if(localStorage.getItem("carrito")) {
 
 //Cargamos catálogo por defecto.
 renderizarCatalogo();
+//Mostramos carrito actual.
+mostrarCarrito();
 
+//***Funciones de filtrado de catálogo***
 //Función para filtrar por busqueda en search.
 const filtrarBusqueda = ()=>{
     canvas.innerHTML = '';
@@ -184,7 +221,7 @@ const filtrarBusqueda = ()=>{
     };
 };
 
-//Filtrar por busqueda en search.
+//Llamamos a la función de busqueda al tipear en search.
 search.addEventListener('keyup', ()=>{
     filtrarBusqueda();
 });
@@ -230,7 +267,7 @@ const filtrarCategoria = (categoria)=>{
 };
 /*HAY QUE REVISAR LA FORMULA Y EL LLAMADO*/
 //Filtrar por categoría de sidebar.
-sidebarArray.addEventListener('click', ()=>{
+sidebarCategorias.addEventListener('click', ()=>{
     let categoria = sidebarArray.options[sidebarArray.selectedIndex].text;
     console.log(categoria);
     filtrarCategoria(categoria);
